76dd64423a2c1bdfa7d869b2e0a2bf2c14fa91fa
David <davidporter33@icloud.com>
2016-11-30 11:41:52 -0800

    added script tags to html, work on my menu which is still not working, and created the constructor function.

---
+++ resume.script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// $(document).ready(function(){
+//   $('#menu').click(function(){
+//     $(this).toggleClass('open');
+//   });
+// });
+
+
+var empHist = [
+  {
+    empName: 'Invitation Homes',
+    empLoc: 'Bellevue, WA',
+    jobTitle: 'Prodject Coordinator'
+    respon: '<p>Direct point of contact for residents with maintenance requests.  Enter data to create maintenance work orders based on resident requests.  Dispatch the work orders to maintenance technicians or assign them to a 3rd party vendors. Manage work orders that are assigned to vendors.  Assist leasing specialists with scheduling home turns for new residents.</p>'
+  },
+  {
+    empName: 'Safelite Autoglass'
+    empLoc: 'Seattle, WA'
+    jobTitle: 'Scheduling Manager'
+    respon: '<p>Take scheduled work order and dispatch those to the 75 technicians.  Provide customer service face to face for customers in the store.  Answer the phones from potential new customers.  Managed glass inventory. Prepared the schedule for the market.  Analyzed market trends to determine the proper amount of hours to schedule.</p>'
+  }
+]
